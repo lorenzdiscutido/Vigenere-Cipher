954ed05599863cedf4419c17b9ca7a71729a568c
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-19 12:46:04 +0800

    Define the function for generating keyword and message

--- Vigenere Cipher.py
+++ Vigenere Cipher.py
@@ -1,1 +1,15 @@
-#Vigenere Cipher
+#Define the function to generate key and message
+def generateKey(message, key):
+    key = list(key)
+    if len(message) == len(key)
+        return(key)
+    else:
+        for i in range(len(message) - len(key))
+         key.append(key[i % len(key)]x)
+    return("" . join(key))
+
+
+
+#Ask the user to enter a message and a key in all caps
+#Encrypt the message using the viginere cipher
+#Print the encrypted message
